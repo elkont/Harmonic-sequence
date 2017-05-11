ffad88ccc26f6fe2189e8b9395e853e1c3d6aec2
elkont <elkont4@yahoo.gr>
2017-05-11 20:43:06 +0300

    Add files via upload
    Calculation of first 5 terms of harmonic sequence.

---
+++ Harmonic1.java
@@ -0,0 +1,14 @@
+class Harmonic1{
+	public static void main(String args[]){
+
+		int num = 1;
+		double result = 0.0;
+
+		while(num <= 5){
+			result = result + (double) 1 / num;
+			num++;
+
+		System.out.println("Output of Harmonic Series is "+result);
+		}
+		}
+	}
